52c8d0d9c0523a51f8ad8715b26f63ae197184cd
Mauricio Arrieta <arrieta1251@gmail.com>
2023-12-21 15:29:00 -0300

    pusheo del jueves 21 diciembre

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
 function Header() {
-  return <h1>HEADERRRRR</h1>;
+  return (
+    <header style={{ backgroundColor: "red" }}>
+      <nav
+        style={{
+          display: "flex",
+          width: "100%",
+        }}
+      >
+        <div>
+          <ul>
+            <li>
+              <a href="#" style={{ color: "white" }}>
+                Inicio{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#" style={{ color: "white" }}>
+                Acerca
+              </a>
+            </li>
+            <li>
+              <a href="#" style={{ color: "white" }}>
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
